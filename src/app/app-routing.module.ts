b640272887fe305517a4c305e9f50741ae3e152c
Lucas Araújo <lucas.compufc@gmail.com>
2021-03-01 21:22:14 -0300

    Crian do rotas para página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonListComponent } from './components/person-list/person-list.component';
+import { PersonDetailComponent } from './components/person-detail/person-detail.component';
+import { AddPersonComponent } from './components/add-person/add-person.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'person-list', component: PersonListComponent },
+  { path: 'person/:id', component: PersonDetailComponent },
+  { path: 'add', component: AddPersonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
